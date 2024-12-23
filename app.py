8b9b8a966b493e08492b7bb538a6a76e33224175
prajwal-ck <ck.prajwal@isteer.com>
2024-12-23 14:49:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
 import process_request
 import uvicorn
+
 app = FastAPI()
 
-# Serve static files from the "static" directory
 @app.get("/")
 def homepage():
     try:
         result = process_request.main()
-        # Execute the Python file and capture its output
-        # if result.returncode == 0:
-        return HTMLResponse(content=f"<h2>Script executed successfully!</h2><pre>{result}</pre>")
-        # else:
-        #     return HTMLResponse(content=f"<h2>Script execution failed!</h2><pre>{result.stderr}</pre>")
+        # Return the result in JSON format
+        return JSONResponse(content={"message": "Script executed successfully!", "result": result})
     except Exception as e:
-        return HTMLResponse(content=f"<h2>An error occurred:</h2><pre>{str(e)}</pre>")
+        # Return error details in JSON format
+        return JSONResponse(content={"message": "An error occurred.", "error": str(e)}, status_code=500)
 
 if __name__ == "__main__":
     uvicorn.run(app)
